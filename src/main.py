afbfabc01e8629b90921db70c8af0eb3445d28c1
nh916 <navidhariri25@gmail.com>
2026-04-23 12:45:12 -0700

    added ability to get the AI API key from  `.env` in the main file

--- src/main.py
+++ src/main.py
@@ -3,6 +3,9 @@ Entry point for the application.
 """
 
 import os
+from pathlib import Path
+
+from dotenv import load_dotenv
 
 from ai.ai_pdf_reader import PDFChat
 
@@ -11,7 +14,16 @@ def main() -> None:
     """
     Entry point for the application.
     """
-    pdf_chat: PDFChat = PDFChat(api_key=os.getenv("ANTHROPIC_API_KEY"))
+
+    # get the API key from the environment variables `.env`
+    load_dotenv(dotenv_path=Path(__file__).resolve().parent.parent / ".env")
+    anthropic_api_key: str = os.getenv("ANTHROPIC_API_KEY")
+
+    # exception handling for missing API key
+    if not anthropic_api_key:
+        raise ValueError("ANTHROPIC_API_KEY is not set")
+
+    pdf_chat: PDFChat = PDFChat(api_key=anthropic_api_key)
 
     pdf_path: str = input("Enter the path to the PDF file: ")
     pdf_chat.load_pdf(pdf_path=pdf_path)
