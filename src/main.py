a6bae98a1c187032853337f5da1e8f8438cd8a96
nh916 <navidhariri25@gmail.com>
2026-04-23 12:40:00 -0700

    wrote first commit for main interface to chat with the AI about PDF via terminal

--- src/main.py
+++ src/main.py
@@ -2,11 +2,20 @@
 Entry point for the application.
 """
 
+import os
+
+from ai.ai_pdf_reader import PDFChat
+
 
 def main() -> None:
     """
     Entry point for the application.
-    Currently, just prints a message to the console.
     """
+    pdf_chat: PDFChat = PDFChat(api_key=os.getenv("ANTHROPIC_API_KEY"))
 
-    print("Hello, World!")
+    pdf_path: str = input("Enter the path to the PDF file: ")
+    pdf_chat.load_pdf(pdf_path=pdf_path)
+
+    while True:
+        user_question: str = input("Enter your question: ")
+        print(pdf_chat.ask(question=user_question))
